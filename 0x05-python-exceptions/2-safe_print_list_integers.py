17174fab76af5e92d3011c37e00355b3e3ec060e
kiggund <abdulbasitkiggund@gmail.com>
2023-11-20 15:05:46 -0800

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
     for i in range(0, x):
@@ -8,4 +10,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
     print("")
-    Return(ret)
+    return (ret)
