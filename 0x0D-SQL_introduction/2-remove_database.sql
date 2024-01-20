82a5d25489548312130d71bb986af3945a235e2b
kiggund <abdulbasitkiggund@gmail.com>
2024-01-20 11:53:52 -0800

    my files

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+0-list_databases.sql
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+3-list_tables.sql
+-- Lists all rows of the table first_table in my MySQL server.
+SELECT * FROM `first_table`;
